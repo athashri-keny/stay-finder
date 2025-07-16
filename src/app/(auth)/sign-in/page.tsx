409ea97bf97c0625ee4784900a2dca331f87486f
Athashri Keny <athashrikeny38@gmail.com>
2025-07-16 13:26:42 +0530

    setup folder structure

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function signIn() {
+  return (
+    <div>
+     <p>SignIn</p>
+    </div>
+  )
+}
+
+export default signIn
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>
+      <p>Sign-In</p>
+    </div>
+  )
+}
+
+export default Signup
